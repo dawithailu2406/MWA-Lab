589273ca042d8eb6831d09de5a218af692f5e7a8
dawihailu2406 <dawit.hailu2406@gmail.com>
2021-01-13 08:57:33 -0600

    added

--- Lab2/Lab22/api/controllers/controllerOfSum.js
+++ Lab2/Lab22/api/controllers/controllerOfSum.js
@@ -1,12 +1,11 @@
 module.exports.sumNumber=function(req,res){
     const intiger1=parseInt(req.params.num1);
     const intiger2=parseInt(req.query.num2);
-    console.log("i am at "+intiger1);
-    console.log("i am at "+intiger1);
-
+    console.log("the first intiger is  "+intiger1);
+    console.log("the second intiger is  "+intiger2);
     const totalSum=intiger1+intiger2;
     const total="The total sum is  "+totalSum;
     console.log(total);
     res.status(200).json(total);
-}
+};
 
--- Lab2/Lab22/api/route/index.js
+++ Lab2/Lab22/api/route/index.js
@@ -2,4 +2,4 @@ const express= require("express");
 const router= express.Router();
 const Sum =require("../controllers/controllerOfSum.js");
 router.route("/:num1/add").get(Sum.sumNumber);
-module.exports=router
+module.exports=router;
