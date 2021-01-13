9653ec1ed0744b9e73c008d05bb7aee2446ae7e5
dawihailu2406 <dawit.hailu2406@gmail.com>
2021-01-13 09:00:42 -0600

    added

--- Lab2/Lab22/app.js
+++ Lab2/Lab22/app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const routes= require("./api/route");
 const app=express();
-app.set("port",5000);
+app.set("port",9090);
 app.use("/lab2",routes);
 const server=app.listen(app.get("port"),function(){
     const port=server.address().port;
