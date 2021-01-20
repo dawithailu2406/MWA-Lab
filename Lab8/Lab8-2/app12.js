0e860e32d895cf2c7a545079f7db1e5ba2405a1a
dawihailu2406 <dawit.hailu2406@gmail.com>
2021-01-20 03:04:52 -0600

    lab 8 is added

---
+++ Lab8/Lab8-2/app12.js
@@ -0,0 +1,29 @@
+require("./api/data/db");
+
+var express=require("express");
+
+var path=require("path");
+var routes=require("./api/routes");
+const bodyParser=require("body-parser");
+
+var app=express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.set("port",4000);
+
+app.use(function(req,res,next){
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use("/node_modules",express.static(path.join(__dirname,"node_modules")))
+
+
+
+app.use("/api",routes);
+
+const server =app.listen(app.get("port"),function(){
+    var port= server.address().port;
+    console.log("Listnening to port" + port);
+
+})
---
+++ Lab8/Lab8-2/public/angular-app/game-list/game-list-controller.js
@@ -0,0 +1,20 @@
+angular.module("meanGames").
+controller("GamesController", GamesController);
+
+function GamesController(GameDataFactory){
+    var vm = this;
+    vm.companyName = "MEAN Games App";
+
+    GameDataFactory.getAllGames()
+        .then(function(response){
+            vm.games = response;
+        });
+}
+// function GamesController($http) {
+//     var vm= this;
+//     vm.title= "Mean Games App";
+//     $http.get("/api/games").
+//     then(function(response){
+//     vm.games= response;
+//     })
+// }
