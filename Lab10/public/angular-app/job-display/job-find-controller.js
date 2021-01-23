9b4dfea274663a881aa1f370d08cb3a6c7de15e1
dawihailu2406 <dawit.hailu2406@gmail.com>
2021-01-22 21:27:06 -0600

    lab 10 is added

--- Lab10/public/angular-app/job-display/job-find-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module("jobApp").controller("JobController", JobController);
-function JobController($routeParams, DataJobFactory) {
-  var id = $routeParams.id;
-  var vm = this;
-  DataJobFactory.getOneJob(id).then(function (response) {
-    vm.job = response;
-  })
-}
